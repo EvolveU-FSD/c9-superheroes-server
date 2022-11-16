61b00765c6cd02655ce9b642687a6aa4ef4e9cf0
Greg Fenton <greg.fenton@gmail.com>
2022-11-16 14:50:00 -0500

    initial superheroes model

---
+++ db/models/superheroModel.js
@@ -0,0 +1,14 @@
+import mongoose from '../mongoose.js';
+
+const Schema = mongoose.Schema;
+
+const superheroSchema = new Schema({
+  name: { type: String, required: true, unique: true },
+  alterEgo: { type: String, default: 'Unknown' },
+  powers: [String],
+  weaknesses: [String],
+  sidekicks: [{ name: String, alterEgo: String }],
+  location: {type: { city: String, province: String, country: String }, required: false}
+});
+
+const Superhero = mongoose.model('Superhero', superheroSchema);
