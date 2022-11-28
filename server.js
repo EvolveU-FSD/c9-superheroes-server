b861175732ceef255ca8ae8579f7d1f98581cad4
Greg Richardson <greg.nmr@gmail.com>
2022-11-28 16:28:51 -0700

    feat: oidc authentication using auth0

--- server.js
+++ server.js
@@ -4,13 +4,39 @@ import myConfig from 'dotenv';
 myConfig.config();
 import DEBUG from 'debug';
 import superheroRouter from './routes/superheroRoutes.js';
+import { auth } from 'express-openid-connect';
 
 export const debug = DEBUG('server:routes');
 debug.enabled = true;
 const PORT = 5001;
 const app = express();
 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SESSION_SECRET,
+  baseURL: process.env.OIDC_BASE_URL,
+  clientID: process.env.OIDC_CLIENT_ID,
+  issuerBaseURL: process.env.OIDC_ISSUER_BASE_URL,
+  routes: {
+    login: false,
+    callback: '/api/callback',
+    logout: '/api/logout'
+  }
+};
+
+app.use(auth(config));
+
 app.use(express.json());
+
+app.get('/api/login', (req, res) =>
+  res.oidc.login({ returnTo: '/superheroes' })
+);
+
+app.get('/api/profile', (req, res) => {
+  const profile = req.oidc.user;
+  res.send(profile);
+});
 
 app.get('/slow', (req, res) => {
   console.log('delay for 3 seconds');
